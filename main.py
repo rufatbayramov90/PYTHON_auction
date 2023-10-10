4bb60c1dea6b0ad7d8e5f5ffef3324bae680bca2
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-10 15:59:00 +0400

    winner of auxion

--- main.py
+++ main.py
@@ -20,20 +20,32 @@ print(student_grades)
 '''
 
 from replit import clear
-
 from art import logo
 print(logo)
 
 bids ={}
 bidding_finish = False
 
+def find_high_bidder(bidding_record):
+    high_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > high_bid:
+            high_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${high_bid}")
+
+
+
 while not bidding_finish:
     name = input("What is your name?")
-    price = input("What is your bid?  $")
+    price = int(input("What is your bid?  $"))
     bids[name] = price
     should_continue = input("Are there any other bidders? yes or no")
     if should_continue == "no":
         bidding_finish = True
+        find_high_bidder(bids)
     elif should_continue == "yes":
         clear()
 
